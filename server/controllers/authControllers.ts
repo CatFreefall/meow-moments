fd287dc5e46a332b05db224ffb209a8be3c48a6a
kevin <kevinfng12@gmail.com>
2023-07-06 18:00:25 -0400

    passwords salted and hashed, and stored in server. backend registration complete.

--- server/controllers/authControllers.ts
+++ server/controllers/authControllers.ts
@@ -1,4 +1,6 @@
 import { Request, Response } from "express";
+import { genSalt, hash } from "bcrypt";
+
 import pool from "../db";
 import {
   registerUser,
@@ -7,48 +9,38 @@ import {
 } from "../queries/authQueries";
 
 // TODO: add e-mail verification
-// duplicate emails and usernames need to be checked before adding the new user to the database.
-// so query the database for the username and email given before adding the user.
 const addUser = (req: Request, res: Response) => {
-  const {
-    username,
-    password_hash,
-    account_creation_date,
-    last_posted,
-    email,
-    first_name,
-    last_name,
-  } = req.body;
+  const { username, password, email, account_creation_date } = req.body;
 
+  // querying to ensure the username DNE in the database
   pool.query(usernameExists, [username], (err, results) => {
     if (results.rows.length) {
       return res.status(409).json("Username already exists");
-    } else {
-      pool.query(emailExists, [email], (err, results) => {
-        if (results.rows.length) {
-          return res.status(409).json("Email already exists");
-        } else {
-          pool.query(
-            registerUser,
-            [
-              username,
-              password_hash,
-              account_creation_date,
-              last_posted,
-              email,
-              first_name,
-              last_name,
-            ],
-            (err, results) => {
-              if (err) {
-                throw err;
-              }
-              res.status(201).json("User successfully added");
-            }
-          );
-        }
-      });
     }
+
+    // querying to ensure the email DNE in the database
+    pool.query(emailExists, [email], (err, results) => {
+      if (results.rows.length) {
+        return res.status(409).json("Email already exists");
+      }
+
+      // asynchronously generating a salt and hashing the password before querying
+      const saltRounds = 10;
+      (async () => {
+        const salt = await genSalt(saltRounds);
+        const password_hash: String = await hash(password, salt);
+        pool.query(
+          registerUser,
+          [username, password_hash, salt, email, account_creation_date],
+          (err, results) => {
+            if (err) {
+              throw err;
+            }
+            res.status(201).json("User successfully added");
+          }
+        );
+      })();
+    });
   });
 };
 
--- server/queries/authQueries.ts
+++ server/queries/authQueries.ts
@@ -1,5 +1,5 @@
 const registerUser =
-  "INSERT INTO users (username, password_hash, account_creation_date, last_posted, email, first_name, last_name) VALUES ($1, $2, $3, $4, $5, $6, $7)";
+  "INSERT INTO users (username, password_hash, password_salt, email, account_creation_date) VALUES ($1, $2, $3, $4, $5)";
 const usernameExists = "SELECT * FROM users WHERE username = $1";
 const emailExists = "SELECT * FROM users WHERE email = $1";
 
