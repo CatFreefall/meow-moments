ab0717369add03a5a8c7224ece4329eb98e68f13
kevin <kevinfng12@gmail.com>
2023-07-08 00:49:16 -0400

    confirmation emails now send if 1. a user tries signing in on an unverified account, and 2. a user registers an account.

--- server/controllers/authControllers.ts
+++ server/controllers/authControllers.ts
@@ -1,5 +1,6 @@
 import { Request, Response } from "express";
 import { genSalt, hash, compare } from "bcrypt";
+import nodemailer from "nodemailer";
 
 import pool from "../db";
 import {
@@ -7,6 +8,32 @@ import {
   usernameExists,
   emailExists,
 } from "../queries/authQueries";
+
+// TODO: add a "didn't recieve the email, click to resend" function
+const transporter = nodemailer.createTransport({
+  service: "gmail",
+  port: 465,
+  secure: true,
+  auth: {
+    user: process.env.NODE_MAILER_HOST,
+    pass: process.env.NODE_MAILER_PASSWORD,
+  },
+});
+
+// TODO: create a proper email template and change link
+// helper function to send a confirmation email to the user
+const sendConfirmationEmail = (email: string): void => {
+  const emailDetails: object = {
+    from: process.env.NODE_MAILER_HOST,
+    to: email,
+    subject: "Confirm Your Meow Moments Account! 🐱",
+    html: `
+    <div>Hello, please confirm your account by clicking the link below.</div>
+    <a href="http://localhost:3000/confirm/${email}">Confirm Account</a>
+    `,
+  };
+  transporter.sendMail(emailDetails);
+};
 
 // TODO: add e-mail verification. this can be done using JWT and setting the token expiration
 // to a short time period (30 minutes).
@@ -20,9 +47,9 @@ const addUser = async (req: Request, res: Response): Promise<any> => {
     const emailQuery = await pool.query(emailExists, [email]);
 
     if (userQuery.rows.length) {
-      return res.status(409).json("Username/Email Already Exists");
+      return res.status(409).json("Username Already Exists");
     } else if (emailQuery.rows.length) {
-      return res.status(409).json("Username/Email Already Exists");
+      return res.status(409).json("Email Already Exists");
     } else {
       // asynchronously generating a salt and hashing the password before querying
       const saltRounds = 10;
@@ -35,15 +62,21 @@ const addUser = async (req: Request, res: Response): Promise<any> => {
         email,
         account_creation_date,
       ]);
-      res.status(201).json("User successfully added");
+      res
+        .status(201)
+        .json(
+          "Confirmation email sent. Please check your email and verify your account."
+        );
+      sendConfirmationEmail(email);
     }
   } catch (err) {
     res.status(500).json("User registration error");
   }
 };
 
+// TODO: rate limit login attempts
 // this function is used to login a user with username/email and password.
-const loginUser = (req: Request, res: Response) => {
+const loginUser = (req: Request, res: Response): void => {
   const { username, email, password } = req.body;
 
   // querying  to ensure the username/email exists in the database
@@ -55,13 +88,25 @@ const loginUser = (req: Request, res: Response) => {
         const password_hash = findUser.rows[0].password_hash;
         const isCorrectPassword = await compare(password, password_hash);
 
+        // sending back repsponses based on status codes.
+        // responses for invalid password and invalid username/email are the same
+        // to prevent users from finding out if a username/email exists in the database.
         if (isCorrectPassword) {
-          res.status(201).json("User successfully logged in");
+          if (findUser.rows[0].is_verified === false) {
+            res
+              .status(401)
+              .json(
+                "User is not verified. Please verify your account using the confirmation email sent."
+              );
+            sendConfirmationEmail(findUser.rows[0].email);
+          } else {
+            res.status(201).json("User successfully logged in");
+          }
         } else {
-          res.status(401).json("Username/Email and/or Password Combo Incorrect");
+          res.status(401).json("Password incorrect");
         }
       } else {
-        res.status(401).json("Username/Email and/or Password Combo Incorrect");
+        res.status(401).json("Username/Email does not exist");
       }
     } catch (err) {
       res.status(500).json("Server error");
--- server/routes/authRoutes.ts
+++ server/routes/authRoutes.ts
@@ -7,4 +7,6 @@ router.post("/register", addUser);
 
 router.post("/login", loginUser);
 
+router.get("/confirm/:token", loginUser);
+
 export default router;
