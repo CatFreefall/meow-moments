cd90bbb05a5b9a05dd9064f443f113f2ac94413b
kevin <kevinfng12@gmail.com>
2023-07-18 23:13:21 -0400

    created basic side menu that can hide and show on click of a button.

--- client/src/components/common/Navbar.tsx
+++ client/src/components/common/Navbar.tsx
@@ -1,5 +1,42 @@
 import { useEffect, useState } from "react";
 
+const Menu = () => {
+  const [showMenu, setShowMenu] = useState(false);
+
+  const MenuButton = () => (
+    <div>
+      <button onClick={() => setShowMenu(true)}>
+        <img
+          src="../../../assets/icons/menu-icon.webp"
+          alt=""
+          className="w-12 ml-4"
+        />
+      </button>
+    </div>
+  );
+
+  const MenuContainer = () => {
+    return (
+      <div className="fixed top-0 left-0 bg-darkgrey h-screen w-3/4 transition duration-700 ease out">
+        temp
+      </div>
+    );
+  };
+
+  return (
+    <div>
+      <MenuButton />
+      <div
+        className={`fixed top-0 left-0 bg-darkgrey h-screen w-3/4 transition duration-700 ease out ${
+          showMenu ? console.log(showMenu) : "-translate-x-full"
+        }`}
+      >
+        <button onClick={() => setShowMenu(false)}>Go Back</button>
+      </div>
+    </div>
+  );
+};
+
 const Navbar = () => {
   const [showNavBar, setShowNavBar] = useState(true);
   const [previousPosition, setPreviousPosition] = useState(window.pageYOffset);
@@ -25,11 +62,7 @@ const Navbar = () => {
         showNavBar ? "translate-y-0" : "-translate-y-full"
       } transition duration-300 ease-out`}
     >
-      <img
-        src="../../../assets/icons/menu-icon.webp"
-        alt=""
-        className="w-12 ml-4"
-      />
+      <Menu />
       <img src="../../../pet.webp" alt="" className="w-20 ml-7 mr-3" />
       <h1 className="text-5xl">MeowMoments</h1>
     </nav>
