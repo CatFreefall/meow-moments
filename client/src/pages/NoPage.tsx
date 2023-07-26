37ab2eda746bedbbe9e253922a6e15cb6c8d6d40
kevin <kevinfng12@gmail.com>
2023-07-26 15:32:48 -0400

    page not found page styled and complete.

--- client/src/pages/NoPage.tsx
+++ client/src/pages/NoPage.tsx
@@ -1,15 +1,22 @@
-import { useNavigate } from "react-router-dom";
+import LoginButton from "../components/common/LoginButton";
+import RegisterButton from "../components/common/RegisterButton";
+import Navbar from "../components/navbar/Navbar";
 
 function NoPage() {
-  const nav = useNavigate();
-
   return (
-    <>
-      <div>Error 404: Not found</div>
-      <button onClick={() => nav("/")}>Home</button>
-      <button onClick={() => nav("/login")}>Log In</button>
-      <button onClick={() => nav("/register")}>Register</button>
-    </>
+    <div className="h-screen flex flex-col justify-center">
+      <Navbar />
+      <div className="mx-5 text-center text-4xl font-header tracking-widest leading-tight">
+        <span className="text-orange">Un-furr-tunately,</span> this page is
+        empty! Head back with these!
+      </div>
+      <div className="flex flex-wrap space-y-4 space-x-2 self-center">
+        <div />
+        <LoginButton />
+        <RegisterButton />
+        <div />
+      </div>
+    </div>
   );
 }
 
