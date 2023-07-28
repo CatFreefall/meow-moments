87ec7385996cde4957108097bfa33ab8dc18c895
kevin <kevinfng12@gmail.com>
2023-07-27 23:29:27 -0400

    dropdown menu now functional and determines which directory the files will be stored in. (illustration, photo, or video)

--- server/controllers/bucket/post.ts
+++ server/controllers/bucket/post.ts
@@ -4,11 +4,13 @@ const post = async (req: any, res: any) => {
   try {
     if (req.files.length > 0) {
       const fileArray: Array<File> = req.files;
-      const blobArray: Array<any> = [];
 
       // uploading all files given by the uer to the bucket one at a time
       for (let fileIndex = 0; fileIndex < fileArray.length; fileIndex++) {
-        const blob = meowMomentsBucket.file(req.files[fileIndex].originalname);
+        // creating the path the file will be stored in in the bucket (with the file name)
+        const fileDestination = `${req.params.postType}/${req.files[fileIndex].originalname}`;
+
+        const blob = meowMomentsBucket.file(fileDestination);
         const blobStream = blob.createWriteStream();
         blobStream
           .on("finish", () => {
@@ -19,9 +21,9 @@ const post = async (req: any, res: any) => {
           })
           .end(req.files[fileIndex].buffer);
       }
-      res.status(200).json("file(s) uploaded to bucket");
+      res.status(200).json("file uploading complete");
     } else {
-      res.status(400).json({ message: "file not provided" });
+      res.status(400).json("file not provided");
     }
   } catch (error) {
     res.status(500).json(error);
