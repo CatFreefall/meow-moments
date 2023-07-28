b627f2e77751aae8bbfbd7baf3e33167ccd99e81
kevin <kevinfng12@gmail.com>
2023-07-27 22:40:52 -0400

    fixed bug where only the first file the user added would be uploaded

--- server/controllers/bucket/post.ts
+++ server/controllers/bucket/post.ts
@@ -3,10 +3,12 @@ import { meowMomentsBucket } from "../../utils/bucketUtils";
 const post = async (req: any, res: any) => {
   try {
     if (req.files.length > 0) {
-      const fileArray = req.files;
+      const fileArray: Array<File> = req.files;
+      const blobArray: Array<any> = [];
 
-      await fileArray.map((file: File) => {
-        const blob = meowMomentsBucket.file(req.files[0].originalname);
+      // uploading all files given by the uer to the bucket one at a time
+      for (let fileIndex = 0; fileIndex < fileArray.length; fileIndex++) {
+        const blob = meowMomentsBucket.file(req.files[fileIndex].originalname);
         const blobStream = blob.createWriteStream();
         blobStream
           .on("finish", () => {
@@ -15,14 +17,14 @@ const post = async (req: any, res: any) => {
           .on("error", (error) => {
             console.log(error);
           })
-          .end(req.files[0].buffer);
-      });
+          .end(req.files[fileIndex].buffer);
+      }
       res.status(200).json("file(s) uploaded to bucket");
     } else {
       res.status(400).json({ message: "file not provided" });
     }
   } catch (error) {
-    res.status(500).json(error + "ok");
+    res.status(500).json(error);
   }
 };
 
